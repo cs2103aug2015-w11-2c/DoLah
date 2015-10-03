d134afc262cc06ffd2c744535ea9bab09a4228dc
starkipraggy <starkipraggy@hotmail.com>
2015-10-03 21:17:56 +0800

    Implemented AddTask logic
    Unit Test skeleton written

--- DoLah/Models/Calendar.cpp
+++ DoLah/Models/Calendar.cpp
@@ -1,36 +1,44 @@
 #include "Calendar.h"
 
 namespace DoLah {
+
 	Calendar::Calendar() {
 	}
 	
 	Calendar::~Calendar() {
-		//TODO
+	}
+
+	std::vector<AbstractTask*> Calendar::getTaskList() {
+		return taskList;
 	}
 
 	void Calendar::addTask(AbstractTask* task) {
+		if (taskList.empty()) {
+			task->setId(1);
+		}
+		else {
+			task->setId(taskList.back()->getId()+1);
+		}
+		taskList.push_back(task);
+	}
+
+	void Calendar::deleteTask(int id) {
+		
+	}
+
+	void Calendar::updateTask(AbstractTask*) {
 		//TODO
 	}
 
-	void deleteTask(int id) {
-		//TODO
+	void Calendar::clearTasks() {
+
 	}
 
-	void updateTask(AbstractTask*) {
-		//TODO
-	}
-
-	AbstractTask* getTask(int id) {
-		//TODO
-
-		return new FloatingTask;
+	AbstractTask* Calendar::getTask(int id) {
+		return *(std::find(taskList.begin(), taskList.end(), id));
 	}
 	
-	std::vector<AbstractTask> search(std::string) {
-		std::vector<AbstractTask> results;
-
-		//TODO
-
-		return results;
+	std::vector<AbstractTask*> Calendar::search(std::string query) {
+		
 	}
 }
