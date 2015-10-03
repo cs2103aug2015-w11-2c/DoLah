d0ae390b78fb9b9b5fabe455b506df05752ad36f
bsmmoon <bsmmoon@gmail.com>
2015-10-03 14:03:25 +0800

    parseSearch partial. invalid argument exceptions added at CommandParser.

--- DoLah/CommandParser.cpp
+++ DoLah/CommandParser.cpp
@@ -17,16 +17,45 @@ void CommandParser::parseAdd(std::vector<std::string> inputArr) {
 }
 
 
+std::string CommandParser::parseSearch(std::string input) {
+    int pos = input.find(" ");
+    std::string arg = input.substr(pos, input.size() - pos);
+}
+
+
 void CommandParser::parse(std::string input) {
     std::vector<std::string> inputArr = ParserLibrary::explode(input, " ");
     std::string command = tokenizer.findCommand(inputArr);
     if (command == ADD) {
+        if (inputArr.size() == 1) {
+            throw std::invalid_argument(TOO_LITTLE_ARGUMENTS_MESSAGE);
+        }
         parseAdd(inputArr);
-    } else if (command == EDIT) {
     } else if (command == SEARCH) {
-    } else if (command == CLEAR) {
+        if (inputArr.size() == 1) {
+            throw std::invalid_argument(TOO_LITTLE_ARGUMENTS_MESSAGE);
+        }
+        parseSearch(input);
+    } else if (command == EDIT) {
+        if (inputArr.size() == 1) {
+            throw std::invalid_argument(TOO_LITTLE_ARGUMENTS_MESSAGE);
+        } else if (inputArr.size() > 2) {
+            throw std::invalid_argument(TOO_MANY_ARGUMENTS_MESSAGE);
+        }
     } else if (command == DELETE) {
+        if (inputArr.size() == 1) {
+            throw std::invalid_argument(TOO_LITTLE_ARGUMENTS_MESSAGE);
+        } else if (inputArr.size() > 2) {
+            throw std::invalid_argument(TOO_MANY_ARGUMENTS_MESSAGE);
+        }
+    } else if (command == CLEAR) {
+        if (inputArr.size() > 1) {
+            throw std::invalid_argument(TOO_MANY_ARGUMENTS_MESSAGE);
+        }
     } else if (command == UNDO) {
+        if (inputArr.size() > 1) {
+            throw std::invalid_argument(TOO_MANY_ARGUMENTS_MESSAGE);
+        }
     }
 }
 
--- DoLah/CommandParser.h
+++ DoLah/CommandParser.h
@@ -13,13 +13,18 @@ public:
 
     void parse(std::string);
     void parseAdd(std::vector<std::string>);
+    std::string parseSearch(std::string);
 private:
     CommandTokenizer tokenizer;
+    
     std::string ADD = "add";
     std::string EDIT = "edit";
     std::string SEARCH = "search";
     std::string CLEAR = "clear";
     std::string DELETE = "delete";
     std::string UNDO = "undo";
+
+    std::string TOO_MANY_ARGUMENTS_MESSAGE = "Too many arguments";
+    std::string TOO_LITTLE_ARGUMENTS_MESSAGE = "Too little arguments";
 };
 
