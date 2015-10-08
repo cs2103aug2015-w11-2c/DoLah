05beb40035dd01c7a44eb2eed5b967aaab00dbfb
Eugene Yeo <yeo.eugene.oey@gmail.com>
2015-10-08 20:15:21 +0800

    Separate UI and CLI project

--- DoLahUI/main.cpp
+++ DoLahUI/main.cpp
@@ -1,17 +1,10 @@
 #include "DoLahUI.h"
-#include "DoLahCLI.h"
 
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2) {
-        QApplication app(argc, argv);
-        DoLah::DoLahUI window;
-        window.show();
-        FreeConsole();
-        return app.exec();
-    } else {
-        startConsole();
-        return 0;
-    }
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    DoLah::DoLahUI window;
+    window.show();
+    FreeConsole();
+    return app.exec();
 }
