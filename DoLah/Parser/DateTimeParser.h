f56b835e5ed4e07cc76ca4331e9e8af05fc7c423
bsmmoon <bsmmoon@gmail.com>
2015-10-17 16:52:31 +0800

    comment on DateTimeParser.h

--- DoLah/Parser/DateTimeParser.h
+++ DoLah/Parser/DateTimeParser.h
@@ -22,16 +22,23 @@ namespace DoLah {
         static int getYear(std::string);
         static std::string tmToString(std::tm);
         static std::tm classifyDate(std::vector<std::string>);
+        // 25th December 2015
         static std::tm checkDMY(std::vector<std::string>);
+        // December 25th 2015
         static std::tm checkMDY(std::vector<std::string>);
     private:
         static int REJECT;
+        // this week, next week (not implemented)
         static std::string relativePattern;
         static std::string datePattern;
         static std::string dayPattern;
-        static std::string dayAppendixPattern;
         static std::vector<std::string> monthPattern;
+
+        // 1st, 2nd, 3rd, 4th
+        static std::string dayAppendixPattern;
+        // 25th of December 2015
         static std::vector<std::string> decorators;
+        // 25/12/2015, 25-12-2015, 25.12.2015,
         static std::vector<std::string> dateDividers;
     };
 }
