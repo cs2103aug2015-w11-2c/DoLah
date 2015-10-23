82dbd6264e8d30a9a47365b868e4d62cf06a80d7
Yan Ling <A0114391@nus.edu.sg>
2015-10-24 04:37:39 +0800

    fixed glitchy jumping window dragging thing

--- DoLahUI/MenuLabel.cpp
+++ DoLahUI/MenuLabel.cpp
@@ -13,6 +13,17 @@ namespace DoLah {
 
     void MenuLabel::mousePressEvent(QMouseEvent* event) {
         emit clicked();
+        if (event->button() == Qt::LeftButton) {
+            //dragPosition = event->globalPos() - static_cast<QMainWindow*>((parent())->parent()->parent())->frameGeometry().topLeft();
+            event->accept();
+        }
+    }
+
+    void MenuLabel::mouseMoveEvent(QMouseEvent *event) {
+        if (event->buttons() & Qt::LeftButton) {
+            //static_cast<QMainWindow*>((parent())->parent()->parent())->move(event->globalPos() - dragPosition);
+            event->accept();
+        }
     }
 
 }
