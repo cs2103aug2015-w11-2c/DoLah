6d484d3d6bdd6b70b0c610d387123a63b675dcdd
starkipraggy <starkipraggy@hotmail.com>
2015-11-08 16:21:02 +0800

    documentation started on Calendar.h

--- DoLah/Models/Calendar.h
+++ DoLah/Models/Calendar.h
@@ -6,14 +6,24 @@
 #include <Models/CommandHistory.h>
 #include "TimeManager.h"
 
+//@@collate Wilson
+
 namespace DoLah {
+    /** \class Calendar
+     *  \brief Class for holding the actual tasklist(s) and any operations on tasks in those list(s).
+     *  
+     *  This class holds all the tasks and contains all the possible operations on the tasklists. Any operation on the tasklists
+     *  must necessarily go through one of the methods in this class.
+     *  For all practical purposes only one copy of this class should exist in the program at any point of time.
+     *  It is not a singleton class only because of testing purposes and also encapsulation problems arising from creating a singleton class.
+     */
 	class Calendar {
 	public:
 		Calendar();
 		~Calendar();
 
-		AbstractTask* getTask(int) const;
-        AbstractTask* getDone(int) const;
+		AbstractTask* getTask(int) const; ///< Retrieve the task at the given index in the main task list taskList.
+        AbstractTask* getDone(int) const; ///< Retrieve the task at the given index in the done task list doneList.
 		std::vector<AbstractTask*> getTaskList() const;
         std::vector<AbstractTask*> Calendar::getDoneList() const;
         std::vector<AbstractTask*> getSearchedTaskList() const;
