bae612e3b2c99533b765056f3b764877cc76f6ec
starkipraggy <starkipraggy@hotmail.com>
2015-11-09 21:15:48 +0800

    CalendarInverter docs

--- DoLah/Models/CalendarInverter.h
+++ DoLah/Models/CalendarInverter.h
@@ -39,8 +39,31 @@ namespace DoLah {
          * @param calendar The calendar to operate on.
          */
         static void invertEdit(AbstractTask *, Calendar *);
+
+        /**
+         * @brief Performs the inverse of the Clear operation on the given Calendar.
+         *
+         * @param taskList The vector of tasks that were deleted.
+         * @param calendar The calendar to operate on.
+         */
         static void invertClear(std::vector<AbstractTask*>, Calendar *);
+
+        /**
+         * @brief Performs the inverse of the Search operation on the given Calendar.
+         * Currently not in use.
+         *
+         * @param taskList The string to search for.
+         * @param calendar The calendar to operate on.
+         */
         static void invertSearch(std::string, Calendar *);
+
+        /**
+         * @brief Performs the inverse of the setDone/setUndone operation on 
+         * the given Calendar.
+         *
+         * @param task The task that had the done flag toggled.
+         * @param calendar The calendar to operate on.
+         */
         static void invertDone(AbstractTask *, Calendar *);
 
     private:
